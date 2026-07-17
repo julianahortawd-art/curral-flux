6e08fe3ac4baddc57c63789eafb34dde79699c96
julianahortawd-art <julianahortawd@gmail.com>
2026-07-17 09:21:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'curralflux-v3';
+const CACHE_NAME = 'curralflux-v4';
 const ASSETS = [
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
